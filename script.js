856d190210f4c326633d6af8ec4e0bd22f554579
dhivi <mdhiva887@gmail.com>
2025-05-16 15:58:37 +0530

    java script add

---
+++ script.js
@@ -0,0 +1,20 @@
+var a=10;
+var b=5;         
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+function add(){
+    var a=10;
+    var b=30;
+    consol.log(a+b)
+}
+add()
+function add(a,b){
+    console.log(a+b)
+}
+add(20,30)
+add(50,30)
+add(10,30)

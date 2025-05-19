9d7d37e3fb0c761b67a3a4850f0c79c6a712bf5b
dhivi <mdhiva887@gmail.com>
2025-05-19 10:21:49 +0530

    first commit

--- script.js
+++ script.js
@@ -9,12 +9,43 @@ console.log(a%b)
 function add(){
     var a=10;
     var b=30;
-    consol.log(a+b)
+    consol.log(a*b)
 }
-add()
-function add(a,b){
-    console.log(a+b)
+function multi(a,b){
+    console.log(a*b)
 }
-add(20,30)
-add(50,30)
-add(10,30)
+multi(20,30)
+multi(50,30)
+multi(10,30)
+var a=5;
+var b='5';
+console.log(a==b)
+
+var a = 10
+var b = 20
+console.log(`${a}+${b}=${a+b}`)
+
+
+n=i
+while(n<=10){
+if(n%2==0)
+{
+    console.log(`${n}even`)
+}
+else
+{
+console.log(`${n} odd`)
+        
+}
+}
+
+
+
+for(n=1;n<=10;n++){
+    if(n%2==0){
+     console.log(`${n}is even`)
+}
+else{
+console.log(`${n}is odd`)
+}
+}

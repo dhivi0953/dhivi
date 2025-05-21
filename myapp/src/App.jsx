775a1b8077903cfd4a437fb9a067394c7f109370
dhivi <mdhiva887@gmail.com>
2025-05-21 10:25:23 +0530

    third commit

---
+++ myapp/src/App.jsx
@@ -0,0 +1,13 @@
+import Dhivi from "./dhivi"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Dhivi/>
+    </>
+  )
+}
+
+export default App
---
+++ myapp/src/Dhivi.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import audio from './audio/malarey.mp3'
+
+function dhivi() {
+  return (
+    <div>
+       <audio src={audio} controls></audio>
+    </div>
+
+  )
+}
+
+export default dhivi

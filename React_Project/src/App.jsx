321fc01c2b8c85f6cab685894bd9563831a1508f
dhivi <mdhiva887@gmail.com>
2025-05-23 16:02:59 +0530

    day9 commit

--- React_Project/src/App.jsx
+++ React_Project/src/App.jsx
@@ -1,42 +1,62 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Child from "./Child"
 import Child1 from './Child1'
+import Home from "./Home"
 
 
-  
- function App() {
-//   // const users = [
-//     // { id: 1, name: "dhivi", age: 25, dept:"computer science"},
-//     // { id: 2, name: "abi", age: 23, dept:"computer science" },
-//     // { id: 3, name: "deepi", age: 22, dept:"computer science" },
-//     // { id: 4, name: "natchu", age: 21, dept:"computer science" },
-//     // { id: 5, name: "guru", age: 26, dept:"computer science" },
-    
-//   // ]
+
+function App() {
+  //   // const users = [
+  //     // { id: 1, name: "dhivi", age: 25, dept:"computer science"},
+  //     // { id: 2, name: "abi", age: 23, dept:"computer science" },
+  //     // { id: 3, name: "deepi", age: 22, dept:"computer science" },
+  //     // { id: 4, name: "natchu", age: 21, dept:"computer science" },
+  //     // { id: 5, name: "guru", age: 26, dept:"computer science" },
+
+  //   // ]
 
 
-//   // const [state, setState] = useState(10)
-//   // const handleClick = () => {
-//     // setState(state + 1)
+  //   // const [state, setState] = useState(10)
+  //   // const handleClick = () => {
+  //     // setState(state + 1)
 
-    // const[name,setName]=useState("")
-    // function handleClick(){
-    //   alert("call from event")
-    // }
-  
-// const[state,setState]=useState("")
+  // const[name,setName]=useState("")
+  // function handleClick(){
+  //   alert("call from event")
+  // }
+
+  // const[state,setState]=useState("")
 
 
-const[name,setName]=useState("")
-function handleClick(data){
-  setName(data)
-}
+  // const[name,setName]=useState("")
+  // function handleClick(data){
+  //   setName(data)
+  // }
+
+  const [users, setUsers] = useState([])
+  function data() {
+    const res = fetch("https://dummyjson.com/product")
+      .then((res) => res.json())
+      .then((data) => setUsers(data.products))
+      .catch((err) => console.log("err", err))
+  }
+
+  useEffect(() => {
+    data()
+  }, [])
+
+
+
+
+
   return (
-    
+
+
     <>
-    
-{/* 
+
+      {/* {
       <button onClick={handleClick}>event trigger </button>
       <br/>
       <input
@@ -46,16 +66,25 @@ function handleClick(data){
     />
     <p>Hii,{name}</p> */}
 
-{/*     
+      {/*     
     <Child state={state}/>
     <Child1 setState={setState}/> */}
 
 
-    <div className="container"></div>
+      {/* <div className="container"></div>
     <div className="row mt-8"></div>
     <div className="col-12 text-center mt-5"></div>
     <Child onButtonClick={handleClick} name={name}/>
-    <h1>{name}</h1>
+    <h1>{name}</h1> */}
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/child" element={<Child users={users} />}>
+
+          </Route>
+        </Routes>
+      </Router>
     </>
 
 
--- React_Project/src/Child.jsx
+++ React_Project/src/Child.jsx
@@ -1,32 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import App from './App'
 
 
-function Child({onButtonClick, name}) {
-    const sentData=()=>{
-        onButtonClick("this message from child!!")
-    }
-    // console.log({users})
-     return (
-//         <>
-//           <div>
-//                 {users.map((user) => (
+// function Child({onButtonClick, name}) {
+//     const sentData=()=>{
+//         onButtonClick("this message from child!!")
+//     }
+//     // console.log({users})
+//      return (
+// //         <>
+// //           <div>
+// //                 {users.map((user) => (
                     
-//                         <ol key={user.id}>
-//                             <li>{user.name}</li>
-//                             <li>{user.age}</li>
-//                             <li>{user.dept}</li>
-//                         </ol>
-// ))}
+// //                         <ol key={user.id}>
+// //                             <li>{user.name}</li>
+// //                             <li>{user.age}</li>
+// //                             <li>{user.dept}</li>
+// //                         </ol>
+// // ))}
 
-// <div>{state}</div>
+// // <div>{state}</div>
 
-<div><button onClick={sentData} className='btn btn-danger'>clickMe</button></div>
+// <div><button onClick={sentData} className='btn btn-danger'>clickMe</button></div>
 
          
           
 
-    )
+//     )
+// }
+
+// export default Child
+
+function Child({users}) {
+    useEffect(()=>{
+        console.log("enter in child component")
+    },[users])
+
+
+
+  return (
+    <div className='text center'>
+        {users.map(user=>(
+            <h1 key={user.id}>{user.title}</h1>
+        
+      ))  }
+      </div>
+  )
 }
+  
 
 export default Child
